423cf6620ae721760370d412d7645576fbc60a1f
BHowland-FoodEnthusiast <brandon.howland@vtsd.com>
2025-01-17 09:00:24 -0500

    My files aren't showing up on the server for some god-unknown reason.

--- welcome/welcome.php
+++ welcome/welcome.php
@@ -1,4 +1,23 @@
-<?php include 'header.php'; ?>
+<?php include 'header.php';
+
+//setup our variables and empty them
+$name = $email = "";
+
+//condition to detect form data and clean it
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+}
+
+//function to sanitize
+function sanitize_input($data){
+    //trim method removes spaces
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+}
+
+?>
 
 <div class="col">
     <div class="row">
